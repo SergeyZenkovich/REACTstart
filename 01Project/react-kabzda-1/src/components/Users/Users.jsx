bfce8f70e09bc077867b624e9b1c4dd755533121
unknown <sapocalipsys.z@gmail.com>
2020-09-03 18:35:58 +0300

    add request to serverwith axios

--- 01Project/react-kabzda-1/src/components/Users/Users.jsx
+++ 01Project/react-kabzda-1/src/components/Users/Users.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import User from './User/User';
+import Axios from 'axios';
+
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoURl: '', followed: false, fullName: 'Luka', status: 'Nooooo!', country: 'Russia', city: 'Rostov' },
-            { id: 2, photoURl: '', followed: true, fullName: 'Romchik', status: 'Skkrrartudu!', country: 'Belarus', city: 'Minsk' },
-            { id: 3, photoURl: '', followed: true, fullName: 'Vlados', status: 'EEEEE!', country: 'Belarus', city: 'Minsk' },
-            { id: 4, photoURl: '', followed: true, fullName: 'Ponchik', status: 'SPS!', country: 'Belarus', city: 'Volkovysk' }
-        ]);
+        Axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response)=>{props.setUsers(response.data.items);});
+        
     }
 
 
-    let usersOnPage = props.users.map(user => <User key={user.id} id={user.id} followed={user.followed} fullName={user.fullName} status={user.status} country={user.country} city={user.city} followUser={props.followUser} unfollowUser={props.unfollowUser} />);
+    let usersOnPage = props.users.map(user => <User key={user.id} id={user.id} followed={user.followed} fullName={user.name} status={user.status} country={user.country} city={user.city} followUser={props.followUser} unfollowUser={props.unfollowUser} />);
     return (
         <div>
             {usersOnPage}
@@ -21,3 +19,10 @@ const Users = (props) => {
 }
 
 export default Users;
+ //Users default 
+//  [
+//     { id: 1, photoURl: '', followed: false, fullName: 'Luka', status: 'Nooooo!', country: 'Russia', city: 'Rostov' },
+//     { id: 2, photoURl: '', followed: true, fullName: 'Romchik', status: 'Skkrrartudu!', country: 'Belarus', city: 'Minsk' },
+//     { id: 3, photoURl: '', followed: true, fullName: 'Vlados', status: 'EEEEE!', country: 'Belarus', city: 'Minsk' },
+//     { id: 4, photoURl: '', followed: true, fullName: 'Ponchik', status: 'SPS!', country: 'Belarus', city: 'Volkovysk' }
+// ]
