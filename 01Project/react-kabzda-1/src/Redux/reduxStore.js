14eec95455e8779661b4c78b066285796df1e8c8
unknown <sapocalipsys.z@gmail.com>
2020-10-13 17:44:47 +0300

    feat: add redux-form

--- 01Project/react-kabzda-1/src/Redux/reduxStore.js
+++ 01Project/react-kabzda-1/src/Redux/reduxStore.js
@@ -6,6 +6,7 @@ import sidebarReducer from "./sidebarReducer"
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from "redux-form";
 
 
 
@@ -15,7 +16,8 @@ let reducers = combineReducers({
     dialogs: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunk));
--- 01Project/react-kabzda-1/src/components/Login/Login.jsx
+++ 01Project/react-kabzda-1/src/components/Login/Login.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 
-const Login=(props)=>{
+const Login = (props) => {
+    const onSubmit = (formData) =>{console.log(formData)} 
     return (
-        <h1>Loging page</h1>
+        <div>
+            <h1>Loging page</h1>
+            <LoginReduxForm onSubmit= {onSubmit} />
+        </div>
     )
 }
+
+const LoginForm = (props) => {
+    return (
+        <form action="" onSubmit ={props.handleSubmit} >
+            <div>
+                <Field type="text" placeholder="login" name="login" component ={"input"} />
+            </div>
+            <div>
+                <Field type="text" placeholder="password" name="password" component ={"input"} />
+            </div>
+            <div>
+                <Field type="checkbox" name="rememberMe"  component ={"input"}/>
+                remember me
+                </div>
+            <div>
+                <button>Log in</button>
+            </div>
+        </form>
+    )
+}
+const LoginReduxForm = reduxForm({
+    form: 'loginForm' 
+})(LoginForm)
 
 export default Login;
