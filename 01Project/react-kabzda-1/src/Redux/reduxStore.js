ae1c00613b30b4e90ea29e8be6aaedde0eb092bb
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2020-10-31 01:50:49 +0400

    feat: add extentions

--- 01Project/react-kabzda-1/src/Redux/reduxStore.js
+++ 01Project/react-kabzda-1/src/Redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 
 import profileReducer from './profileReducer';
 import dialogsReducer from "./dialogsReducer";
@@ -6,7 +6,7 @@ import sidebarReducer from "./sidebarReducer"
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunk from "redux-thunk";
-import {reducer as formReducer} from "redux-form";
+import { reducer as formReducer } from "redux-form";
 import appReducer from "./appReducer";
 
 
@@ -22,6 +22,11 @@ let reducers = combineReducers({
     form: formReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
-window.store = store;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+
+// let store = createStore(reducers, applyMiddleware(thunk));
+window.__store__ = store;
 export default store;
